783d8557e0a64f533131db6de891ba7f1e0da37d
Tarik <tarik.jaidani@gmail.com>
2024-10-12 14:15:03 +0200

    flipper color final

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,21 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color');
+
+function getRandomNumber() {
+    return  colors[Math.floor(Math.random() * 4 )]
+
+}
+
+
+btn.addEventListener("click", ()=>{
+ 
+
+    document.body.style.backgroundColor = getRandomNumber() 
+    color.textContent = getRandomNumber() 
+    
+})
+
+
+
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,28 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color');
+
+
+function getRandomElements(arr, count) {
+    const result = [];
+    
+    for (let i = 0; i < count; i++) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        result.push(arr[randomIndex]);
+        result
+    }
+    return result;
+}
+
+btn.addEventListener("click", ()=>{
+    const primaryColor = getRandomElements(hex, 6).join('')
+ 
+    // Set the CSS variable dynamically
+    document.documentElement.style.setProperty('--primary-color', "#"+ primaryColor);
+    color.textContent = "#"+ primaryColor
+    document.body.style.backgroundColor =  "#"+ primaryColor
+})
+
+
+
