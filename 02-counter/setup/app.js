ea24e39e35bf39ab16c75e9be2e208ee31199128
Tarik <tarik.jaidani@gmail.com>
2024-10-12 16:26:02 +0200

    final counter

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,45 +1,32 @@
 
-const decrease = document.querySelector('.decrease')
-const reset = document.querySelector('.reset')
-const increase = document.querySelector('.increase')
+const btns = document.querySelectorAll('.btn')
 const value =  document.getElementById('value')
-let counter =0
+
+let count =0
 value.textContent =0
 
-
-increase.addEventListener('click', () =>{
-    counter +=1 
-    if (counter >= 1) {
-        value.textContent= counter
-        value.style.color= "green"  
-    } else if (counter <0 )  {
-        value.textContent= counter
-        value.style.color= "red"  
-    } else  {
-        value.textContent= counter
-        value.style.color= "black"
-    }  
-     
+btns.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        const styles = e.currentTarget.classList
+      
+         if (styles.contains("increase")){
+            count ++
+         } else if (styles.contains("decrease")) {
+            count --
+         } else if (styles.contains("reset")) {
+            count =0
+         }
+         
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#222";
+        }
+         value.textContent = count
+         
+    })
 })
-
-decrease.addEventListener('click', () =>{
-    counter -=1 
-    if (counter < 0){
-    value.textContent= counter
-    value.style.color= "red"
-    } else if (counter >= 1)  {
-        value.textContent= counter
-        value.style.color= "green"  
-    } else  {
-        value.textContent= counter
-        value.style.color= "black"
-    } 
-    
-})
-
-reset.addEventListener('click', () =>{
-    counter =0 
-    value.textContent= counter
-     value.style.color= "black"
-})
- 
